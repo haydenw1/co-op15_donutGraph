12813b1491a0cd799cdaebe9a4e62c7ade4a0a42
Hayden Wagner <hbw6955@rit.edu>
2015-07-28 12:55:18 -0400

    Code Structure Change, color changes
    1. code structure changed so its all encapsulated in donut object and
    the initial measurements are all stored as properties of this object
    2. color changes to match style guide
    3. background style now changes on touch event
    4. added background gray ring
    5. made code more readable, still needs more work

--- js/main.js
+++ js/main.js
@@ -1,142 +1,206 @@
-function setUp(){
-  var width = document.documentElement.clientWidth,
-    height = document.documentElement.clientHeight,
-    radius = Math.min(width, height) / 2,
-    outerRadius = radius - 10,
-    innerRadius = radius - (radius/2.25),
-    textBoxHeight = 66;
+var donut = {
+  
+  d3tools: [],
+  elements: [],
+  measurements: [],
 
-  var textDiv = document.createElement("div"),
-      industryText = document.createElement("p"),
-      countText = document.createElement("p"),
-      percentText = document.createElement("p");
+  setUp: function(){
+    donut.measurements.width = document.documentElement.clientWidth,
+    donut.measurements.height = document.documentElement.clientHeight,
+    donut.measurements.radius = Math.min(donut.measurements.width, donut.measurements.height) / 2,
+    donut.measurements.outerRadius = donut.measurements.radius - 10,
+    donut.measurements.innerRadius = donut.measurements.radius - (donut.measurements.radius/2.25),
+    donut.measurements.textBoxHeight = 66;
 
-      textDiv.setAttribute("class","text-div");
-      industryText.setAttribute("class","industry-text");
-      countText.setAttribute("class","count-text");
-      percentText.setAttribute("class","percent-text");
+    donut.elements.div = document.createElement("div");
+    donut.elements.industry = document.createElement("p");
+    donut.elements.count = document.createElement("p");
+    donut.elements.percent = document.createElement("p");
 
-      textDiv.appendChild(industryText);
-      textDiv.appendChild(countText);
-      textDiv.appendChild(percentText);
+    donut.elements.div.setAttribute("class","text-div");
+    donut.elements.industry.setAttribute("class","industry-text");
+    donut.elements.count.setAttribute("class","count-text");
+    donut.elements.percent.setAttribute("class","percent-text");
 
-  var color = d3.scale.ordinal()
-      .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
+    donut.elements.div.appendChild(donut.elements.industry);
+    donut.elements.div.appendChild(donut.elements.count);
+    donut.elements.div.appendChild(donut.elements.percent);
 
-  var arc = d3.svg.arc()
-      .outerRadius(outerRadius)
-      .innerRadius(innerRadius);
+    donut.d3tools.color = d3.scale.ordinal()
+      .range(["#002e6c", "#00adee", "#ff661b", "#025594", "#9fc600", "#be1e2d", "#333333"]);
 
-  var pie = d3.layout.pie()
+    donut.d3tools.arc = d3.svg.arc()
+      .outerRadius(donut.measurements.outerRadius)
+      .innerRadius(donut.measurements.innerRadius);
+
+    donut.d3tools.pie = d3.layout.pie()
       .sort(null)
-      .value(function(d) { return d.percent; });
+      .value(function(d) { 
+        return d.percent; 
+      });
 
-  var svg = d3.select("body").append("svg")
-      .attr("width", width)
-      .attr("height", height)
-    .append("g")
-      .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+    donut.elements.gVis = d3.select("body")
+      .append("svg")
+        .attr("width", donut.measurements.width)
+        .attr("height", donut.measurements.height)
+      .append("g")
+        .attr("transform", "translate(" + donut.measurements.width / 2 + "," + donut.measurements.height / 2 + ")");
 
-  useData();
+    donut.useData();
+  },
+
+  useData: function(){
+    
+    donut.elements.gVis.selectAll("path")
+      .data(donut.d3tools.pie(donut.ringData))
+      .enter()
+      .append("path")
+        .attr("class", "back-ring")
+        .attr("d", donut.d3tools.arc);
+        //.style("fill", "#bbbbbc");
+
+
+    var g = donut.elements.gVis.selectAll(".arc")
+      .data(donut.d3tools.pie(donut.data))
+      .enter()
+      .append("g")
+        .attr("class", "arc");
+
+    g.append("path")
+      .attr("d", donut.d3tools.arc)
+      .style("fill", function(d,i) { return donut.d3tools.color(i); });
+/*
+    g.append("text")
+      .attr("transform", function(d) { return "translate(" + donut.d3tools.arc.centroid(d) + ")"; })
+      .attr("dy", ".35em")
+      .style("text-anchor", "middle")
+      .style("pointer-events", "none")
+      .text(function(d) { return d.data.percent + "%"; });*/
+
+    g.selectAll("path")
+      .on("mouseover", function(d){
+        console.log(this);
+        donut.touchPath(d, this);
+      })
+      .on("mouseout", function(d){
+        donut.leavePath(d, this);
+      });
+
+      donut.elements.div.style.top = String(donut.measurements.height/2 - donut.measurements.textBoxHeight/2) + "px";
+      donut.elements.div.style.left = String(donut.measurements.radius - donut.measurements.innerRadius + 10) + "px";
+      document.body.appendChild(donut.elements.div);
+  },
+
+  touchPath: function(d, touched){
+    d3.select(touched.parentNode.childNodes[1])
+      .transition()
+        .text("");
+
+    d3.select(touched)
+      .transition()
+        .style("fill", "white")
+        .style("opacity", .75);
+
+    d3.select(".industry-text")
+      .text(d.data.industry);
+
+    d3.select(".count-text")
+      .text(d.data.count + " students");
+
+    d3.select(".percent-text")
+      .text("(" + d.data.percent + "% of co-op students)");
+
+    d3.select(".color-background")
+      .transition()
+        .style("background", touched.style.fill);
+
+    if(d.data.article){
+      document.getElementById('p-industry').innerHTML = d.data.industry;
+      document.getElementById('hidden-link').style.bottom = "0px";
+    }else{
+      console.log("doesn't have an article");
+    }
+  },
+
+  leavePath: function(d, touched){
+    d3.select(touched.parentNode.childNodes[1])
+      .transition()
+        .text(function(d) { 
+          return d.data.percent + "%"; 
+        });
+    
+    d3.select(touched)
+      .transition()
+        .style("fill",function(d) {  
+          return donut.d3tools.color(donut.data.indexOf(d.data)); 
+        })
+        .style("opacity", 1);
+
+    d3.select(".industry-text").text(null);
+    d3.select(".count-text").text(null);
+    d3.select(".percent-text").text(null);
+
+    d3.select(".color-background")
+      .transition()
+        .style("background", "white");
+
+    document.getElementById('hidden-link').style.bottom = "-95px";
+  },
+
+  ringData: [
+    {
+      "percent": 100
+    }
+  ],
+
+  data: [  
+    {
+      "industry":"Print",
+      "count":53,
+      "percent":37,
+      "article": 1
+    },
+
+    {
+      "industry":"Agency",
+      "count":21,
+      "percent":15,
+      "article": 1
+    },
+
+    {
+      "industry":"Consumer Products",
+      "count":22,
+      "percent":15,
+      "article": 0
+    },
+
+    {
+      "industry":"Publishing",
+      "count":13,
+      "percent":9,
+      "article": 1
+    },
+
+    {
+      "industry":"Vender",
+      "count":13,
+      "percent":9,
+      "article": 0
+    },
+
+    {
+      "industry":"Other",
+      "count":11,
+      "percent":7,
+      "article": 1
+    },
+
+    {
+      "industry":"Mobile/Software",
+      "count":8,
+      "percent":6,
+      "article": 0
+    }
+  ]
 }
-
-//start json import and elements that need data
-//d3.json("data/data.json", function(error, data){
-//  if(error) return console.log(error);
-data = [	
-	{
-		"industry":"Print",
-		"count":53,
-		"percent":37,
-		"article": 1
-	},
-
-	{
-		"industry":"Agency",
-		"count":21,
-		"percent":15,
-		"article": 1
-	},
-
-	{
-		"industry":"Consumer Products",
-		"count":22,
-		"percent":15,
-		"article": 0
-	},
-
-	{
-		"industry":"Publishing",
-		"count":13,
-		"percent":9,
-		"article": 1
-	},
-
-	{
-		"industry":"Vender",
-		"count":13,
-		"percent":9,
-		"article": 0
-	},
-
-	{
-		"industry":"Other",
-		"count":11,
-		"percent":7,
-		"article": 1
-	},
-
-	{
-		"industry":"Mobile/Software",
-		"count":8,
-		"percent":6,
-		"article": 0
-	}
-]
-
-function useData(){
-  var g = svg.selectAll(".arc")
-    .data(pie(data))
-  .enter().append("g")
-    .attr("class", "arc");
-
-  g.append("path")
-    .attr("d", arc)
-    .style("fill", function(d) { return color(d.data.count); });
-
-  g.append("text")
-    .attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
-    .attr("dy", ".35em")
-    .style("text-anchor", "middle")
-    .style("pointer-events", "none")
-    .text(function(d) { return d.data.percent + "%"; });
-
-  g.selectAll("path")
-    .on("mouseover", function(d){
-      d3.select(this.parentNode.childNodes[1]).transition().text("");
-      d3.select(this).transition().style("opacity",".3");
-      d3.select(".industry-text").text(d.data.industry);
-      d3.select(".count-text").text(d.data.count + " students");
-      d3.select(".percent-text").text("(" + d.data.percent + "% of co-op students)");
-      if(d.data.article){
-        document.getElementById('p-industry').innerHTML = d.data.industry;
-        document.getElementById('hidden-link').style.bottom = "0px";
-      }else{
-        console.log("doesn't have an article");
-      }
-    })
-    .on("mouseout", function(d){
-      d3.select(this.parentNode.childNodes[1]).transition().text(function(d) { return d.data.percent + "%"; });
-      d3.select(this).transition().style("opacity","1");
-      d3.select(".industry-text").text(null);
-      d3.select(".count-text").text(null);
-      d3.select(".percent-text").text(null);
-      document.getElementById('hidden-link').style.bottom = "-95px";
-    });
-
-    textDiv.style.top = String(height/2 - textBoxHeight/2) + "px";
-    textDiv.style.left = String(radius - innerRadius + 10) + "px";
-  document.body.appendChild(textDiv);
-}
-//});
-//end json function
